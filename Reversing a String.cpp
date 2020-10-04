c0e3df0b7b1b22664662d34e4b87bcb907b6cc5b
jeshu54 <60354888+jeshu54@users.noreply.github.com>
2020-10-04 09:45:19 -0400

    A simple technique to reverse a string

---
+++ Reversing a String.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+// I want to make a function that is defined in the class, which will reverse strings.
+
+
+
+
+void reverse(string A, string B, int size) {
+  
+  for(int i=size-1; i>=0; i--) {
+    B[size-i-1] = A[i];
+ }
+  for (int i=0; i<=size-1; i++) {
+    cout << B[i] ;
+}
+}
+
+int main() {
+  int size;
+  string A = "avroismyname";
+  string B;
+  size = A.length();
+  reverse(A,B, size);
+
+}
